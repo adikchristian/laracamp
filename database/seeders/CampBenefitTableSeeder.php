35a3fdf51ef94a9eceb3597dc6c46b2777220af3
Adi Christian <34901010+adikchristian@users.noreply.github.com>
2023-07-19 11:19:32 +0800

    add seeder Camp, Camp Benefit, Admin

---
+++ database/seeders/CampBenefitTableSeeder.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\CampBenefit;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CampBenefitTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $campBenefits = [
+            [
+                'camp_id' => 1,
+                'name' => 'Pro Techstack Kit',
+                'created_at' => \date('Y-m-d', \time()),
+                'updated_at' => \date('Y-m-d', \time()),
+            ],
+            [
+                'camp_id' => 1,
+                'name' => 'iMac Pro 2021 & Display',
+                'created_at' => \date('Y-m-d', \time()),
+                'updated_at' => \date('Y-m-d', \time()),
+            ],
+            [
+                'camp_id' => 1,
+                'name' => '1-1 Mentoring Program',
+                'created_at' => \date('Y-m-d', \time()),
+                'updated_at' => \date('Y-m-d', \time()),
+            ],
+            [
+                'camp_id' => 1,
+                'name' => 'Final Project Certificate',
+                'created_at' => \date('Y-m-d', \time()),
+                'updated_at' => \date('Y-m-d', \time()),
+            ],
+            [
+                'camp_id' => 1,
+                'name' => 'Offline Course Videos',
+                'created_at' => \date('Y-m-d', \time()),
+                'updated_at' => \date('Y-m-d', \time()),
+            ],
+            [
+                'camp_id' => 1,
+                'name' => 'Future Job Opportinity',
+                'created_at' => \date('Y-m-d', \time()),
+                'updated_at' => \date('Y-m-d', \time()),
+            ],
+            [
+                'camp_id' => 1,
+                'name' => 'Premium Design Kit',
+                'created_at' => \date('Y-m-d', \time()),
+                'updated_at' => \date('Y-m-d', \time()),
+            ],
+            [
+                'camp_id' => 1,
+                'name' => 'Website Builder',
+                'created_at' => \date('Y-m-d', \time()),
+                'updated_at' => \date('Y-m-d', \time()),
+            ],
+            [
+                'camp_id' => 2,
+                'name' => '1-1 Mentoring Program',
+                'created_at' => \date('Y-m-d', \time()),
+                'updated_at' => \date('Y-m-d', \time()),
+            ],
+            [
+                'camp_id' => 2,
+                'name' => 'Final Project Certificate',
+                'created_at' => \date('Y-m-d', \time()),
+                'updated_at' => \date('Y-m-d', \time()),
+            ],
+            [
+                'camp_id' => 2,
+                'name' => 'Offline Course Videos',
+                'created_at' => \date('Y-m-d', \time()),
+                'updated_at' => \date('Y-m-d', \time()),
+            ],
+            [
+                'camp_id' => 2,
+                'name' => 'Offline Course Videos',
+                'created_at' => \date('Y-m-d', \time()),
+                'updated_at' => \date('Y-m-d', \time()),
+            ],
+            
+        ];
+
+        CampBenefit::insert($campBenefits);
+    }
+}
